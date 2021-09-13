cffaec34e8f18f12a3494f9b28effaec5ea00618
Akop Kesheshyan <hello@akop.dev>
2021-09-13 04:19:13 +0300

    Fix gcam alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,7 @@
 alias ga="git add"
 alias gaa="git add -A"
 alias gcm="git commit -m "
-alias gcam="git commit -am "
+alias gcam="git add -A && git commit -m"
 alias gs="git status"
 alias gp="git push"
 alias gl="git-log"
