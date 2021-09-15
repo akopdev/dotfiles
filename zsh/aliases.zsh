2878251ed08ba26fecba0ce7ae9800174fd47eff
Akop Kesheshyan <hello@akop.dev>
2021-09-15 13:11:42 +0300

    (zsh) add exa shortcuts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,7 @@ alias ..="cd .."
 alias ~="cd ~"
 alias p="goto-projects"
 alias c="clear"
+alias l="exa --group-directories-first --icons --oneline --all"
+alias ll="exa --group-directories-first --icons --long --all --git"
+alias lt="exa --group-directories-first --icons --tree --all --level=3 --ignore-glob '.git|node_modules|.pytest_cache|__pycache__'"
+
