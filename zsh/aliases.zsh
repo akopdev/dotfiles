847c8e6840ad6610549f01861c853ee06c92f931
Akop Kesheshyan <hello@akop.dev>
2021-09-15 15:19:45 +0300

    (zsh) add search commands

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,4 +7,5 @@ alias c="clear"
 alias l="exa --group-directories-first --icons --oneline --all"
 alias ll="exa --group-directories-first --icons --long --all --git"
 alias lt="exa --group-directories-first --icons --tree --all --level=3 --ignore-glob '.git|node_modules|.pytest_cache|__pycache__'"
-
+alias f="find"
+alias ff="find-file"
