3f098cac86083cc3454d492316c8ebc0517e54a0
Akop Kesheshyan <hello@akop.dev>
2021-10-22 09:04:37 +0100

    Add random password generator

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,7 @@ alias ll="exa --group-directories-first --icons --long --all --git"
 alias lt="exa --group-directories-first --icons --tree --all --level=3 --ignore-glob '.git|node_modules|.pytest_cache|__pycache__'"
 alias f="find"
 alias ff="find-file"
+alias passgen="passgen"
 if [ "$TERM" = "xterm-kitty" ]; then
     alias ssh="kitty +kitten ssh"
 fi
