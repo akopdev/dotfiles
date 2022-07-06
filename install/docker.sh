dacad6f3e7ea4be966725939ec10658445a5026a
Akop Kesheshyan <hello@akop.dev>
2022-07-06 13:58:53 +0100

    feat(shellcheck): add shell script static analysis tool

--- install/docker.sh
+++ install/docker.sh
@@ -3,7 +3,8 @@
 HOME="/home/$(whoami)"
 DOTFILES="${HOME}/.dotfiles"
 
-source ${DOTFILES}/install/common.sh
+# shellcheck source=/dev/null
+source "${DOTFILES}"/install/common.sh
 
 # Setting up local folders ...
 for folder in "Projects" ".config"; do
@@ -13,19 +14,20 @@ for folder in "Projects" ".config"; do
 done
 
 # Installing brew packages
-for brew_package in `cat ${DOTFILES}/install/brew-packages.txt`
+while read -r brew_package; 
 do
-  brew install ${brew_package}
-done
+  brew install "${brew_package}"
+done < "${DOTFILES}"/install/brew-packages.txt
 
 # Installing npm packages
-for npm_package in `cat ${DOTFILES}/install/npm-packages.txt`
+while read -r npm_package;
 do
-  npm i -g ${npm_package}
-done
+  npm i -g "${npm_package}"
+done < "${DOTFILES}"/install/npm-packages.txt
 
 # Run general setup scripts
-for file in $DOTFILES/**/setup.sh
+for file in "$DOTFILES"/**/setup.sh
 do
-  source $file
+  # shellcheck source=/dev/null
+  source "$file"
 done
