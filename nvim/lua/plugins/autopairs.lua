7ebde9397051d320402b17b484e8b03aa6151aaa
Akop Kesheshyan <hello@akop.dev>
2023-04-11 06:08:43 +0200

    (nvim): fix autopairs plugin

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,9 +1,12 @@
-local status_ok, autopairs = pcall(require, "nvim-autopais")
+local status_ok, autopairs = pcall(require, "nvim-autopairs")
 if not status_ok then
     return
 end
 
 autopairs.setup {
+    fast_wrap = {
+        map = '<C-w>',
+    },
     disable_filetype = { 
         "TelescopePrompt" , 
         "vim" 
