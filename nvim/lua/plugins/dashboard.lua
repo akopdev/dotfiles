03ba83a0e68db8e25d4c5a1967c973a66d741a7e
Akop Kesheshyan <hello@akop.dev>
2023-08-03 11:30:19 +0200

    fix(tmux): change color indicator for fullscreen mode and add old apple hello screen to nvim

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -3,6 +3,24 @@ if not status_ok then
     return
 end
 
+local custom_logo = {
+'',
+'',
+'',
+'    ,dPYb,              ,dPYb, ,dPYb,             ',
+'    IP``Yb              IP``Yb IP``Yb             ',
+'    I8  8I              I8  8I I8  8I             ',
+'    I8  8               I8  8" I8  8"             ',
+'    I8 dPgg,    ,ggg,   I8 dP  I8 dP    ,ggggg,   ',
+'    I8dP" "8I  i8" "8i  I8dP   I8dP    dP"  "Y8ggg',
+'    I8P    I8  I8, ,8I  I8P    I8P    i8`    ,8I  ',
+'   ,d8     I8, `YbadP` ,d8b,_ ,d8b,_ ,d8,   ,d8`  ',
+'   88P     `Y8888P"Y8888P`"Y888P`"Y88P"Y8888P"   ',
+'',
+'',
+'',
+}
+
 dashboard.setup {
     theme = 'doom',
     hide = {
@@ -11,9 +29,7 @@ dashboard.setup {
         winbar = false
     },
     config = {
-        week_header = {
-            enable = true,
-        },
+        header = custom_logo,
         center = {
             { desc = 'Load Session',  action = 'SessionLoad',                          key = 'l' },
             { desc = 'Find Text',     action = 'Telescope live_grep',                  key = 'f' },
