a1c66173cb1633bf9c8c25987f59b7a6f29a0edb
Akop Kesheshyan <hello@akop.dev>
2025-01-18 22:04:55 +0100

    Switch exa to maintained fork eza

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,9 @@
 # System
 alias c="clear"
-alias ls="exa --group-directories-first --icons --all"
-alias l="exa --group-directories-first --icons --oneline --all"
-alias ll="exa --group-directories-first --icons --long --all --git"
-alias lt="exa --group-directories-first --icons --tree --all --level=3 --ignore-glob '.git|node_modules|.pytest_cache|__pycache__'"
+alias ls="eza --group-directories-first --icons --all"
+alias l="eza --group-directories-first --icons --oneline --all"
+alias ll="eza --group-directories-first --icons --long --all --git"
+alias lt="eza --group-directories-first --icons --tree --all --level=3 --ignore-glob '.git|node_modules|.pytest_cache|__pycache__'"
 alias f="find-text"
 alias ff="find-file"
 alias top="htop"
