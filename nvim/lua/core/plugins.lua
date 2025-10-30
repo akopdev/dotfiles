2e4ad5adba78b80a379ba2af26c18d954699a392
Akop Kesheshyan <hello@akop.dev>
2025-10-30 17:00:15 +0100

    nvim: add diffview

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -87,6 +87,9 @@ return packer.startup(function(use)
         end
 
     }
+
+    use "sindrets/diffview.nvim"
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
