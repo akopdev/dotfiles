44e449ad22633af343ac268ed76b4268ce6b40ad
Akop Kesheshyan <hello@akop.dev>
2026-01-21 15:15:24 +0100

    nvim: migrate autocmd code to lua

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -2,42 +2,77 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- Save window position on exit
 autocmd("BufWinLeave", {
-    pattern = "*.*",
-    command = "mkview",
+  pattern = "*",
+  callback = function()
+    -- use silent to avoid messages if view can't be made
+    vim.cmd("silent! mkview")
+  end,
 })
+
 -- Restore window position
 autocmd("BufWinEnter", {
-    pattern = "*.*",
-    command = "silent! loadview",
+  pattern = "*",
+  callback = function()
+    vim.cmd("silent! loadview")
+  end,
 })
 
+-- Markdown / text settings
 autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = "*.{md,txt}",
-    command = "setlocal colorcolumn=80 fo=awqtc comments+=nb:> spell"
-        .. " tabstop=2 shiftwidth=2 expandtab nocursorline",
+  pattern = { "*.md", "*.txt" },
+  callback = function()
+    vim.opt_local.colorcolumn = "80"
+    vim.opt_local.formatoptions = "wqc"
+    vim.opt_local.comments:append("nb:>")
+    vim.opt_local.spell = true
+
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+
+    vim.opt_local.cursorline = false
+  end,
 })
 
+-- Treat *.jscs / *.jshint / *.eslintrc as JSON
 autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = "*.{jscs,jshint,eslint}rc",
-    command = "set filetype=json",
+  pattern = { "*.jscsrc", "*.jshintrc", "*.eslintrc" },
+  callback = function()
+    vim.bo.filetype = "json"
+  end,
 })
 
+-- Zsh-related files as shell
 autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = "aliases.local,zshrc.local,.zshrc,*/zsh/*",
-    command = "set filetype=sh",
+  pattern = { "aliases.local", "zshrc.local", ".zshrc", "*/zsh/*" },
+  callback = function()
+    vim.bo.filetype = "sh"
+  end,
 })
 
+-- Git config files
 autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = "gitconfig.local,.gitconfig",
-    command = "set filetype=gitconfig",
+  pattern = { "gitconfig.local", ".gitconfig" },
+  callback = function()
+    vim.bo.filetype = "gitconfig"
+  end,
 })
 
+-- Vim config files
 autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = ".vimrc,init.vim",
-    command = "set filetype=vim",
+  pattern = { ".vimrc", "init.vim" },
+  callback = function()
+    vim.bo.filetype = "vim"
+  end,
 })
 
+-- Python indentation
 autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = "*.py",
-    command = "setlocal tabstop=4 softtabstop=4 shiftwidth=4 modifiable"
+  pattern = "*.py",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.bo.modifiable = true
+  end,
 })
